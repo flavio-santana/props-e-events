728018c7fa4f4f96416ed7e6bbd9ff09a112cd88
Flavio Santana <f.o.santana@gmail.com>
2020-02-29 00:12:37 -0300

    inserindo mais um FilmItemListId

--- src/components/FilmesListaIten.vue
+++ src/components/FilmesListaIten.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="list-group-item">
         <span>Vingadores: Guerra Infinita</span>
-        <button class="btn btn-success float-right">Editar</button>
+        <button class="btn btn-success float-right" title="Editar">Editar</button>
     </li>
 </template>
