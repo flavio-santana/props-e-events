7a67fa97cd4f44341731e4731a6336e9a053029e
Flavio Santana <f.o.santana@gmail.com>
2020-04-14 16:48:05 -0300

    Código restaurado

--- src/components/FilmesLista.vue
+++ src/components/FilmesLista.vue
@@ -16,8 +16,7 @@
         <FilmesListaIten 
           v-for="filme in filmes" 
           :key="filme.id" 
-          :filme="filme"
-          @selecionarFilme="filmeSelecionado = $event"/>
+          :filme="filme"/>
         
       </div>
     </div>
@@ -51,9 +50,7 @@ export default {
         {id: 3, titulo: 'Era Uma Vez em… Hollywood', ano: 2018, duracao: 360, diretor: 'Flávio Santana'},
         {id: 4, titulo: 'Pantera Negra', ano: 2018, duracao: 158, diretor: 'Flávio Santana'},
         {id: 5, titulo: 'DeadPool 2', ano: 2018, duracao: 177, diretor: 'Flávio Santana'},
-      ],
-      filmeSelecionado: undefined
-      
+      ]  
     }
   }
 }
--- src/components/FilmesListaIten.vue
+++ src/components/FilmesListaIten.vue
@@ -2,12 +2,8 @@
     
     <li class="list-group-item">
         <span>{{ filme.id }} || {{ filme.titulo }} || {{ filme.ano }} || {{ filme.duracao }} || {{ filme.diretor }}</span>
-        <button @click="selecionar" class="btn btn-success float-right" title="Selecionar">Selecionar</button>
+        <button class="btn btn-success float-right" title="Selecionar">Selecionar</button>
     </li>
-    
-    <!--
-    <input type="text" :value="titulo" :placeholder="$attrs.placeholder"> 
-    -->
 
 </template>
 <script>
@@ -19,13 +15,6 @@ export default{
             required: true,
         }
     },
-    methods:{
-        selecionar(event){
-            this.$emit('selecionarFilme',this.filme)
-        }
-    },
-    created(){
-        console.log('Attrs: ' , this.$attrs)
-    }
+
 }
 </script>
